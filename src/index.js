49e6d4f3085d8251a3542e06ed76a8f5780a7446
Seungjun Chae <sjchae@sjchae-Gram17.localdomain>
2023-01-21 21:16:16 -0500

    add comment

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+// NOTE we have used BrowserRouter to keep our UI in sync with the URL. BrowserRouter helps with seamless transitions while switching between components. Basically, it will only reload/refresh the component that needs to be changed instead of refreshing/reloading the entire page. Though BrowserRouter is not a necessity, it is a must if you want your app to be responsive.
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
